c8f3d1d36155f700b7862b3f9fd8fc2a6e735da1
KamalRajput <34761964+KamalRajput@users.noreply.github.com>
2018-03-19 17:07:08 +0530

    Add files via upload
    use command mvn spring-boot:run to start the application.
    And that’s it! We now have a fully REST-compliant CRUD server!

---
+++ src/main/java/CrudInterface/StudentRepository.java
@@ -0,0 +1,10 @@
+package CrudInterface;
+
+import POJO.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface  StudentRepository  extends CrudRepository<Student, Long>{
+
+}
